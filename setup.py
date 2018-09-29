04899a446d1d8c47155515e8e2cd02279d23a243
Jacobi Petrucciani <8117202+jpetrucciani@users.noreply.github.com>
2018-09-29 13:56:06 -0400

    pypi deployment fixes from pyclickup

--- setup.py
+++ setup.py
@@ -2,17 +2,40 @@
 """
 pip setup file
 """
-from pybugsnag.globals import __version__, LIBRARY
 from setuptools import setup, find_packages
 
+LIBRARY = "pybugsnag"
 
 with open("README.rst") as readme:
     long_description = readme.read()
 
 
+with open("requirements.txt") as requirements:
+    install_requires = requirements.read().split("\n")
+    install_requires = [x.strip() for x in install_requires if x.strip()]
+
+def find_version(*file_paths):
+    import os
+    import re
+    """
+    This pattern was modeled on a method from the Python Packaging User Guide:
+        https://packaging.python.org/en/latest/single_source_version.html
+    We read instead of importing so we don't get import errors if our code
+    imports from dependencies listed in install_requires.
+    """
+    base_module_file = os.path.join(*file_paths)
+    with open(base_module_file) as f:
+        base_module_data = f.read()
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                              base_module_data, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
+
 setup(
     name=LIBRARY,
-    version=__version__,
+    version=find_version('pybugsnag', 'globals.py'),
     description="A python wrapper for the Bugsnag Data Access API",
     long_description=long_description,
     author="Jacobi Petrucciani",
@@ -22,7 +45,7 @@ setup(
     download_url="https://github.com/jpetrucciani/{}.git".format(LIBRARY),
     license="LICENSE",
     packages=find_packages(),
-    install_requires=["requests"],
+    install_requires=install_requires,
     classifiers=[
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.5",
